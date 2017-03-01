27728493dfdeb7d40476279423e71722401d1c3f
Joseph Tran <fred.smoth@gmail.com>
2017-03-01 11:06:37 +1100

    Evolving and devolving stats scale back down appropriately, trying to sort out what happens if you get damaged and then devolve

--- js/models/animals.js
+++ js/models/animals.js
@@ -1,8 +1,8 @@
 function Animal(options) {
     this.name = options.name;
-	
-	this.evolvedStats = options.evolvedStats;
-	
+    
+    this.evolvedStats = options.evolvedStats;
+    
     this.stats = this.evolvedStats[0];
 
     if (options.isPet !== undefined)
@@ -18,37 +18,71 @@ function Animal(options) {
     this.type = options.type;
 
     this.resetStats = options.resetStats;
-	
-	this.resetAllStats = options.resetAllStats;
+    
+    this.resetAllStats = options.resetAllStats;
 }
 
 function Stats(hp, attack, speed, hpBuff, attackBuff, speedBuff, maxLevel, blockBonus, escapeBonus) {
-	this.hp = hp;
-	this.attack = attack;
-	this.speed = speed;
-	
-	this.hpBuff = hpBuff;
-	this.attackBuff = attackBuff;
-	this.speedBuff = speedBuff;
-	
-	this.currentLevel = 0;
-	this.maxLevel = maxLevel;
-	
-	this.blockBonus = blockBonus;
-	this.escapeBonus = escapeBonus;
-	
-	this.evolveStats = function() {
-		// buff the stats in accordance with the level and buffs
-	}
-	
-	this.devolveStats = function() {
-		// be sure to round evenly off when devolving
-		// goal is that you "take less damage" whilst evolved
-	}
-	
-	this.walkingHeal = function() {
-		// heal whilst walking
-	}
+    this.currentStats = {
+        hp: hp,
+        attack: attack,
+        speed: speed
+    };
+
+    this.originalStats = {
+        hp: hp,
+        attack: attack,
+        speed: speed
+    };
+
+    this.buffs = {
+        hp: hpBuff,
+        attack: attackBuff,
+        speed: speedBuff,
+        block: blockBonus,
+        escape: escapeBonus
+    };
+
+    this.currentLevel = 1;
+    this.maxLevel = maxLevel;
+
+    this.evolveStats = function() {
+        // buff the stats in accordance with the level and buffs
+        if (this.currentLevel < this.maxLevel) {
+            this.currentLevel++;
+            this.currentStats.hp *= this.buffs.hp;
+
+            this.currentStats.speed += this.buffs.speed;
+
+            console.log(this.currentStats);
+        }
+    };
+    
+    this.devolveStats = function() {
+        // be sure to round evenly off when devolving
+        // goal is that you "take less damage" whilst evolved
+        if (this.currentLevel > 0) {
+            var remainderHp = this.currentStats.hp % this.originalStats.hp;
+            if (remainderHp > 0) {
+                this.currentStats.hp -= remainderHp;
+            }
+
+            this.currentStats.hp /= ((this.currentLevel) * this.buffs.hp);
+            console.log(this.currentStats);
+            this.currentStats.attack = this.originalStats.attack;
+            this.currentStats.speed = this.originalStats.speed;
+
+            this.currentLevel = 0;
+        }
+
+        // aim to reset back to original state, but hp drops to whatever the equivalent is
+    };
+    
+    this.walkingHeal = function() {
+        // heal whilst walking
+        if (this.currentStats.hp < this.originalStats.hp)
+            this.currentStats.hp++;
+    };
 }
 
 function generateAnimalStats(hp, attk, spd, state) {
@@ -62,7 +96,7 @@ function generateAnimalStats(hp, attk, spd, state) {
             this.attk = attk;
             this.spd = spd;
             this.state = state;
-			//console.log(this);
+            //console.log(this);
         }
     };
 }
@@ -70,33 +104,32 @@ function generateAnimalStats(hp, attk, spd, state) {
 var cat = new Animal({
     name: "CAT",
     //stats: generateAnimalStats(10, 10, 2, EVOLUTION_STATES.BASIC),
-	evolvedStats: [
-		generateAnimalStats(10, 10, 2, EVOLUTION_STATES.BASIC),
-		generateAnimalStats(20, 15, 5, EVOLUTION_STATES.CHAMPION),
-		generateAnimalStats(35, 20, 10, EVOLUTION_STATES.ULTIMATE)
-	],
+    evolvedStats: [
+        generateAnimalStats(10, 10, 2, EVOLUTION_STATES.BASIC),
+        generateAnimalStats(20, 15, 5, EVOLUTION_STATES.CHAMPION),
+        generateAnimalStats(35, 20, 10, EVOLUTION_STATES.ULTIMATE)
+    ],
     type: ANIMAL_TYPES.CAT
 });
 
 var duck = new Animal({
     name: "DUCK",
     //stats: generateAnimalStats(10, 12, 1, EVOLUTION_STATES.BASIC),
-	evolvedStats: [
-		generateAnimalStats(10, 12, 1, EVOLUTION_STATES.BASIC),
-		generateAnimalStats(18, 2, 6, EVOLUTION_STATES.CHAMPION),
-		generateAnimalStats(27, 22, 9, EVOLUTION_STATES.ULTIMATE)
-	],
-	type: ANIMAL_TYPES.DUCK,
-	resetAllStats: function() {
-		for(var i = 0; i < this.evolvedStats.length; i++) {
-			this.evolvedStats[i].resetStats();
-			console.log("Resetting", this.evolvedStats[i]);
-		}
-	}
+    evolvedStats: [
+        generateAnimalStats(10, 12, 1, EVOLUTION_STATES.BASIC),
+        generateAnimalStats(18, 2, 6, EVOLUTION_STATES.CHAMPION),
+        generateAnimalStats(27, 22, 9, EVOLUTION_STATES.ULTIMATE)
+    ],
+    type: ANIMAL_TYPES.DUCK,
+    resetAllStats: function() {
+        for(var i = 0; i < this.evolvedStats.length; i++) {
+            this.evolvedStats[i].resetStats();
+            console.log("Resetting", this.evolvedStats[i]);
+        }
+    }
 });
 
-/*
 function BiomeState(state, animals) {
-	this.state = state;
-	
-}*/
+    this.state = state;
+    
+}
